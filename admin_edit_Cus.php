17b6a4380633990caa9d92e7faf230f9bd3947fc
Kawin Waiwingrob <61160084@go.buu.ac.th>
2022-04-13 04:30:14 +0700

    Edit user profile, v2

--- admin_edit_Cus.php
+++ admin_edit_Cus.php
@@ -210,7 +210,7 @@ if (isset($_REQUEST['btn_update'])) {
                             </a>
                             <!-- Dropdown - User Information -->
                             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-                                <a class="dropdown-item" href="#">
+                                <a class="dropdown-item" href="admin_profile.php">
                                     <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
                                     Profile
                                 </a>
